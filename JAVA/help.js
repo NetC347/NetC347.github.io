d3814b11b602584358937f818fe7c1e8d84a95c6
NetC347 <143455608+NetC347@users.noreply.github.com>
2023-09-14 12:49:28 -0500

    removed clock

--- JAVA/help.js
+++ JAVA/help.js
@@ -1,21 +1,3 @@
-window.addEventListener("load", (event) => {
-    getTime()
-});
-
-setInterval(getTime, 1000);
-
-function getTime() {
-    const options = {
-        hourCycle: "h12",
-        timeStyle: "short"
-    };
-    var realtime = new Date().toLocaleString(navigator.languages[0], options);
-    document.getElementById('time').innerText = realtime;
-}
-
-var x = document.getElementById("warn");
-    var y = document.getElementById("links");
-    var z = document.getElementById("pad");
 
 function myClick() {
     var x = document.getElementById("warn");
