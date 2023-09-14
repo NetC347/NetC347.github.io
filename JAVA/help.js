8bbb0af1f2cc1cceefafc46bd57664fefc0c2d57
NetC347 <143455608+NetC347@users.noreply.github.com>
2023-09-14 13:01:07 -0500

    java mistake

--- JAVA/help.js
+++ JAVA/help.js
@@ -1,13 +1,13 @@
 
 function myClick() {
     var x = document.getElementById("warn");
+    var y = document.getElementById("links");
+    var z = document.getElementById("pad");
 
     x.style.display = 'none';
 
 
 if (x.style.display = 'flex') {
-    var y = document.getElementById("links");
-    var z = document.getElementById("pad");
 
     y.style.display = 'none';
     z.style.display = 'none';
